19c8f60e3d6e14f8bcc4fc3d827528abbcf0bc37
demiurgosoft <demiurgosoft@hotmail.com>
2016-09-13 19:54:24 +0100

    exported function

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 XEJS
 ====
+_By @angrykoala_
+**eXtreme EJS**, 
 
-eXtreme EJS
+XEJS allows you to escape-and-override EJS tags on the fly, creating a new totally different tag-based language using only [EJS](https://github.com/mde/ejs)
+
+> Recursive templating, what could go wrong?
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-var ejs=require('ejs');
-var fs=require('fs');
+var ejs = require('ejs');
+var fs = require('fs');
 
-ejs.renderFile("test.ejs", {
-    openTag: "<%",
-    closeTag: "%>",
-    render: ejs.render,
-    fs: fs,
-    tagRegex: /<%/g
-    
-}, function(err,res){
-    console.log(err);
-    fs.writeFile("result.html",res,function(){
-        console.log("finished");
-    });    
-});
+var render = ejs.render;
+
+function xejs(file, options) {
+    try {
+        var content = fs.readFileSync(file, 'utf-8');
+        var regexp = new RegExp(options.openTag + "\\s*include (.+)\\s*" + options.closeTag, "g");
+        content = content.replace(options.tagRegex, options.openTagEJS + "%");
+        content = content.replace(regexp, options.openTagEJS + "- xejs('$1',options)" + options.closeTagEJS);
+        content = render(content, {
+            xejs: xejs,
+            options: options,
+            fs: fs,
+            render: render
+        });
+        return content;
+    } catch (e) {
+        console.log("XEJS error", e);
+        return "";
+    }
+}
+
+module.exports = function(file, options) {
+    var renderingOptions = {
+        openTagEJS: "<%",
+        closeTagEJS: "%>",
+        tagRegex: /<%/g,
+        openTag: options.openTag || "{{",
+        closeTag: options.closeTag || "}}"
+    };
+
+    xejs(file, renderingOptions);
+};
