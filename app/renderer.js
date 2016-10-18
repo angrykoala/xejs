de599118732c5bf46be19df7b50fc498909bb4ed
angrykoala <demiurgosoft@hotmail.com>
2016-10-18 23:37:31 +0100

    small refactor in renderer.js

--- app/renderer.js
+++ app/renderer.js
@@ -2,39 +2,52 @@ var ejs = require('ejs');
 var fs = require('fs');
 var path = require('path');
 
-var render = ejs.render;
+var ejsRenderer = ejs.render;
 
 var tagParser = require('./tag_parser');
 
 
-function getDirname(file, parentPath) {
-    var dirname = file;
-    if (parentPath) dirname = path.join(parentPath, "../", file);
-    return dirname;
+function getFilePath(file, parentPath) {
+    var filePath = file;
+    if (parentPath) filePath = path.join(parentPath, "../", file);
+    return filePath;
 }
 
-function parseContent(dirname, options) {
-    var content = fs.readFileSync(dirname, 'utf-8');
+function loadFile(filePath){
+    return fs.readFileSync(filePath, 'utf-8');
+}
+
+function parseContent(content, options) {
     if (options.ejsEscape !== false) content = content.replace(options.tagRegex, "<%%");
     return tagParser(content, options.tokens, options);
 }
 
-function setupOptions(dirname, options) {
+function setupOptions(filePath, options) {
     var rendererOptions = options.args || {};
     rendererOptions.xejs = xejs;
-    rendererOptions.parentPath = dirname;
+    rendererOptions.parentPath = filePath;
     rendererOptions.options = options;
     return rendererOptions;
 }
 
+
 function xejs(file, options, parentPath) {
-    var dirname = getDirname(file, parentPath);
-    var content = parseContent(dirname, options);
-    var rendererOptions = setupOptions(dirname, options);
-    content = render(content, rendererOptions);
+    var filePath = getFilePath(file, parentPath);
+    var content= loadFile(filePath);
+    content = parseContent(content, options);
+    var rendererOptions = setupOptions(filePath, options);
+    content = ejsRenderer(content, rendererOptions);
     rendererOptions.parentPath = parentPath;
     return content;
 }
 
+function renderFromString(content,options){
+    var filePath = ".";
+    content = parseContent(content, rendererOptions);
+    var rendererOptions = setupOptions(filePath, options);
+    content = ejsRenderer(content, rendererOptions);
+    rendererOptions.parentPath = filePath;
+    return content;
+}
 
 module.exports = xejs;
