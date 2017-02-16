c87c0eae06e2326b7ca423dd24f767b2a58afde8
angrykoala <demiurgosoft@hotmail.com>
2017-02-16 23:57:55 +0000

    Tag Parser refactored into a class

--- app/tag_parser.js
+++ app/tag_parser.js
@@ -1,57 +1,83 @@
 "use strict";
-// Tag manipulation
 
+//Default tags to add
 const defaultTokens = [
     [/include\s+(\S+)/i, "xejs(\"$1\",options,parentPath)"]
 ];
 
+class TagParser {
+    constructor(options) {
+        this.openTagEJS = options.openTagEJS || "<%- ";
+        this.closeTagEJS = options.closeTagEJS || "%>";
+        this.openTag = options.openTag || "{{";
+        this.closeTag = options.closeTag || "}}";
+        this.commentTag = options.commentTag || "#";
+    }
 
-function stripComments(content, options) {
-    const commentOptions = {
-        openTag: options.openTag + options.commentTag,
-        closeTag: options.closeTag
-    };
-    const commentRegex = generateTagRegex(/[\s\S]*/i, commentOptions);
-    content = content.replace(commentRegex, "");
-    return content;
-}
-
-function generateTagRegex(token, options) {
-    let modifier = "g";
-    if (token.ignoreCase) modifier += "i";
-    let tokenString = token.source;
-    tokenString = tokenString.replace(/\\s/g, "\ ");
-    tokenString = "\\ *?" + tokenString + "?\\ *?";
-
-    return new RegExp(options.openTag + tokenString + options.closeTag, modifier);
-}
-
-function escapeToken(input) {
-    return input.replace(/(["'<%>=-])/g, "\\$1");
-}
-
-function replaceTags(content, tokens, options) {
-    function replaceCallback() {
-        let result = options.openTagEJS + command + options.closeTagEJS;
-        for (let i = 1; i < arguments.length - 2; i++) {
-            let elem = escapeToken(arguments[i]);
+    replaceCallback() {
+        let params = Array.prototype.slice.call(arguments);
+        let command=params.shift();
+        let result = this.openTagEJS + command + this.closeTagEJS;
+        for (let i = 1; i < params.length - 2; i++) {
+            let elem = this.escapeToken(params[i]);
             result = result.replace("$" + i, elem);
         }
         return result;
     }
 
+    //Replace tags of content
+    replaceTags(content, tokens) {
+        content = this.stripComments(content);
 
-    content = stripComments(content, options);
-
-    for (let i = 0; i < tokens.length; i++) {
-        const reg = generateTagRegex(tokens[i][0], options);
-        //var required for scope purposes
-        var command = tokens[i][1]; // jshint ignore:line
-        content = content.replace(reg, replaceCallback);
+        for (let i = 0; i < tokens.length; i++) {
+            const reg = this.generateTagRegex(tokens[i][0]);
+            //var required for scope purposes
+            let command = tokens[i][1]; // jshint ignore:line
+            content = content.replace(reg, this.replaceCallback.bind(this,command));
+        }
+        return content;
     }
-    return content;
+
+    // Removes all comments
+    stripComments(content) {
+        const commentRegex = this.generateCommentTagRegex(/[\s\S]*/i);
+        content = content.replace(commentRegex, "");
+        return content;
+    }
+
+    // Generates regex from token
+    generateTagRegex(token) {
+        let modifier = "g";
+        if (token.ignoreCase) modifier += "i";
+        let tokenString = token.source;
+        tokenString = tokenString.replace(/\\s/g, "\ ");
+        tokenString = "\\ *?" + tokenString + "?\\ *?";
+
+        return new RegExp(this.openTag + tokenString + this.closeTag, modifier);
+    }
+    // Generates regex from token
+    generateCommentTagRegex(token) {
+        let modifier = "g";
+        if (token.ignoreCase) modifier += "i";
+        let tokenString = token.source;
+        tokenString = tokenString.replace(/\\s/g, "\ ");
+        tokenString = "\\ *?" + tokenString + "?\\ *?";
+
+        return new RegExp(this.openTag + this.commentTag + tokenString + this.closeTag, modifier);
+    }
+
+    escapeToken(input) {
+        return input.replace(/(["'<%>=-])/g, "\\$1");
+    }
+
 }
 
-replaceTags.defaultTags = defaultTokens;
+function parseTag(content, tokens, options) {
+    const parser = new TagParser(options);
+    const res = parser.replaceTags(content, tokens);
+    return res;
+}
 
-module.exports = replaceTags;
+parseTag.defaultTags = defaultTokens;
+
+module.exports = parseTag;
