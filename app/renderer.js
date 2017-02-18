657df1efdc58d0f0c96d53a1d9e084621076bd84
angrykoala <demiurgosoft@hotmail.com>
2017-02-18 00:25:37 +0000

    Renderer refactored into a class #33

--- app/renderer.js
+++ app/renderer.js
@@ -15,54 +15,75 @@ function getFilePath(file, parentPath) {
     return filePath;
 }
 
-function fileInStack(file,renderedStack){
-    if(renderedStack.indexOf(file)>=0) return true;
-    else return false;
-}
 
-function loadFile(filePath) {
-    return fs.readFileSync(filePath, 'utf-8');
-}
+class Renderer {
 
-function parseContent(content, options) {
-    if (options.ejsEscape !== false) content = content.replace(options.tagRegex, "<%%");
-    return tagParser(content, options.tokens, options);
-}
+    constructor(options) {
 
-function optionsSetup(filePath, options) {
-    const rendererOptions = options.args || {};
-    rendererOptions.xejs = xejs;
-    rendererOptions.parentPath = filePath;
-    rendererOptions.options = options;
-    return rendererOptions;
-}
+        this.tokens = options.tokens;
+        this.renderedStack = [];
 
-//Avoid repeating code
-function xejs(file, options, parentPath) {
-    const filePath = getFilePath(file, parentPath);
-    if(fileInStack(filePath, options.renderedStack)) throw new Error("Error: Found circular dependencies while parsing xejs");
-    options.renderedStack.push(filePath);
-    let content = loadFile(filePath);
-    content = parseContent(content, options);
-    const rendererOptions = optionsSetup(filePath, options);
-    content = ejsRenderer(content, rendererOptions);
-    rendererOptions.parentPath = parentPath;
-    options.renderedStack.pop();
-    return content;
-}
+        this.options = options; //remove in the futuuure
+    }
 
-//Avoid repeating code
-function renderString(content, options, includePath) {
-    includePath = includePath || process.cwd();
-    includePath += "/file";
-    content = parseContent(content, options);
-    const rendererOptions = optionsSetup(includePath, options);
-    content = ejsRenderer(content, rendererOptions);
-    rendererOptions.parentPath = includePath;
-    return content;
+    render(file, x, parentPath) {
+        const filePath = getFilePath(file, parentPath);
+        if (this.fileInStack(filePath)) throw new Error("Error: Found circular dependencies while parsing xejs");
+        this.renderedStack.push(filePath);
+        let content = this.loadFile(filePath);
+        content = this.parseContent(content);
+        const rendererOptions = this.optionsSetup(filePath);
+        content = ejsRenderer(content, rendererOptions);
+        rendererOptions.parentPath = parentPath;
+        this.renderedStack.pop();
+        return content;
+    }
+
+    //Avoid repeating code
+    renderString(content,x, includePath) {
+        includePath = includePath || process.cwd();
+        includePath += "/file";
+        content = this.parseContent(content);
+        const rendererOptions = this.optionsSetup(includePath);
+        content = ejsRenderer(content, rendererOptions);
+        rendererOptions.parentPath = includePath;
+        return content;
+    }
+
+
+    optionsSetup(filePath) {
+        const options = this.options;
+        const rendererOptions = options.args || {};
+        rendererOptions.xejs = this.render.bind(this);
+        rendererOptions.parentPath = filePath;
+        rendererOptions.options = options;
+        return rendererOptions;
+    }
+
+    loadFile(filePath) {
+        return fs.readFileSync(filePath, 'utf-8');
+    }
+
+    parseContent(content) {
+        if (this.options.ejsEscape !== false) content = content.replace(this.options.tagRegex, "<%%"); //this should go to tagRenderer!!
+        return tagParser(content, this.tokens, this.options);
+    }
+
+    fileInStack(file) {
+        if (this.renderedStack.indexOf(file) >= 0) return true;
+        else return false;
+    }
+
 }
 
 module.exports = {
-    renderFile: xejs,
-    renderString: renderString
+    renderFile: function(files, options, parentPath) {
+        const renderer = new Renderer(options);
+        return renderer.render(files, null, parentPath);
+
+    },
+    renderString: function(content, options, includePath) {
+        const renderer = new Renderer(options);
+        return renderer.renderString(content, null, includePath);
+    }
 };
